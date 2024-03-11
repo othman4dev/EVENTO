a6d59391870a698b1c24db60bee6f17c6edd8ea0
Othman4dev <othmandevsup@gmail.com>
2024-03-11 15:05:51 +0000

    Adding cHANGES

--- database/migrations/2024_03_04_102902_events.php
+++ database/migrations/2024_03_04_102902_events.php
@@ -19,11 +19,11 @@ return new class extends Migration
             $table->date('date');
             $table->time('time');
             $table->decimal('price', 8, 2);
-            $table->unsignedBigInteger('category_id');
-            $table->unsignedBigInteger('user_id');
             $table->integer('approved')->default(0);
             $table->integer('deleted')->default(0);
+            $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->rememberToken();
             $table->timestamps();
